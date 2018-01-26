47f0cc2bba447769366ef0e475d224a60d0afe97
brendon-ai <brendon-m@outlook.com>
2018-01-26 10:11:11 -0500

    Convert to integers, delete old images

--- infer.py
+++ infer.py
@@ -40,14 +40,32 @@ parameters.maxThreshold = 100
 # Create the blob detector with the above parameters
 blob_detector = cv2.SimpleBlobDetector_create(parameters)
 
+
+# Function to delete all old images from the temp folder
+def remove_old_images():
+    for file_path in glob.iglob(TEMP_PATH + 'sim*.png'):
+        os.remove(file_path)
+
+
+# Clean images from the folder before starting
+remove_old_images()
+
 # Loop forever (or until a keyboard interrupt)
 while True:
     # List all correctly named image files in the temp folder
     image_names = glob.glob(TEMP_PATH + 'sim*.png')
+
+    # If there are no images, go back to the beginning of the loop
+    if not image_names:
+        continue
+
     # Order them by modification time
     image_names.sort(key=os.path.getmtime)
     # Load the last image in the list
     image = imread(image_names[-1])
+
+    # Now that the image has been read, delete all old images in the folder
+    remove_old_images()
 
     # Slice up the image into windows
     image_slices = view_as_windows(image, (WINDOW_SIZE, WINDOW_SIZE, 3), STRIDE)
@@ -57,13 +75,16 @@ while True:
     predictions = model.predict(image_slices_flat)
     # Reshape the predictions to have the same initial two dimensions as the original list of image slices
     predictions_row_arranged = np.reshape(predictions, image_slices.shape[:2])
+    # Convert the floating-point array to unsigned 8-bit integers, multiplying it by 255 to scale it into the range of 0
+    # to 255, instead of 0 to 1
+    predictions_integer = np.array(predictions_row_arranged * 255, dtype=np.uint8)
 
     # Find blobs in the heat map, which will be located around the location of the stop signs
-    blob_key_points = blob_detector.detect(predictions_row_arranged)
+    blob_key_points = blob_detector.detect(predictions_integer)
     # Convert the key points to tuple positions
     blob_positions = [key_point.pt for key_point in blob_key_points]
     # Convert the points to comma-separated values that each make up one line
-    positions_comma_separated = [position[0] + ' ' + position[1] for position in blob_positions]
+    positions_comma_separated = [position[0] + ',' + position[1] for position in blob_positions]
     # Open a temporary file in the temp folder to write to
     with open(SIGN_TEMP_FILE_PATH, 'w') as temp_file:
         # Print each of the comma-separated lines to the file
