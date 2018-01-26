3f7efcd3cd382114d4f137377680ca03ac5912ba
brendon-ai <brendon-m@outlook.com>
2018-01-26 12:58:12 -0500

    Scale positions to screen size before printing to file

--- infer.py
+++ infer.py
@@ -81,18 +81,20 @@ while True:
     # Convert the floating-point array to unsigned 8-bit integers, multiplying it by 255 to scale it into the range of 0
     # to 255, instead of 0 to 1
     predictions_integer = np.array(predictions_row_arranged * 255, dtype=np.uint8)
-    # Upscale the heat map so it can be blurred more effectively
-    heat_map_large = cv2.resize(predictions_integer, (640, 360))
-    # Blur the scaled heat map using a median filter so that large blobs of pixels will be blurred and more circular,
-    # and noise will be greatly reduced
-    heat_map_blurred = cv2.medianBlur(heat_map_large, 15)
 
     # Find blobs in the heat map, which will be located around the location of the stop signs
     blob_key_points = blob_detector.detect(predictions_integer)
     # Convert the key points to tuple positions
     blob_positions = [key_point.pt for key_point in blob_key_points]
+    # Divide the positions by the size of the heat map to get values from 0 to 1
+    heat_map_shape = predictions_integer.shape
+    heat_map_shape_transpose = [heat_map_shape[1], heat_map_shape[0]]
+    blob_positions_scaled = [
+        [element / size for element, size in zip(position, heat_map_shape_transpose)]
+        for position in blob_positions
+    ]
     # Convert the points to comma-separated values that each make up one line
-    positions_comma_separated = [str(position[0]) + ',' + str(position[1]) for position in blob_positions]
+    positions_comma_separated = [str(position[0]) + ',' + str(position[1]) for position in blob_positions_scaled]
     # Open a temporary file in the temp folder to write to
     with open(SIGN_TEMP_FILE_PATH, 'w') as temp_file:
         # Print each of the comma-separated lines to the file
